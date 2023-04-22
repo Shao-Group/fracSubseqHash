f0f65faecb0324a06417dfcf053c9564fffe699a
Ke Chen <kxc5915@psu.edu>
2023-04-21 20:10:40 -0400

    added doubly linked list as bins according to the heuristic alg of Eades et al

--- makeStringGraph.py
+++ makeStringGraph.py
@@ -86,8 +86,9 @@ degrees of all vertices) and make (hi - lo + 1) + 2
 bins, the additional 2 are for sinks and sources).
 """
 # doubly linked list structure within each bin
-# for each vertex, [prev, next, bin] are both indices
-links = np.full((g.num_vertices(), 3), -1, dtype=np.int32)
+# for each vertex, [prev, next] are both indices
+# of the diff array
+links = np.full((g.num_vertices(), 2), -1, dtype=np.int32)
 
 sinks = (out_w == 0)
 sources = ~sinks & (in_w == 0)
@@ -95,10 +96,94 @@ lo = diff[~sinks & ~sources].min()
 hi = diff[~sinks & ~sources].max()
 num_bins = hi - lo + 3
 bin_shift = 2 - lo
+diff += bin_shift
+diff[sinks] = 0
+diff[sources] = 1
 
 # storing [st, ed] indices for the doubly linked
 # list of each bin, the bins at index 0 and 1 are
 # for sinks and sources, respectively
 bins = np.full((num_bins, 2), -1, dtype=np.int32)
 
+for i in range(diff.size):
+   cur = diff[i]
+   if bins[cur, 0] < 0: #first in bin
+      bins[cur, 0] = bins[cur, 1] = i
+   else: #link to ed
+      links[i, 0] = bins[cur, 1] #i.prev to ed
+      links[bins[cur, 1], 1] = i #olded.next to i
+      bins[cur, 1] = i
 
+# assume bins[i, 0] (i.e., the st pointer of bin i) is not -1
+# detach this element from the doubly linked list
+# cur = st, st -> st.next, st.prev = -1, cur.next = -1
+# and return it
+def dll_rm_first(i, bins, links):
+   cur = bins[i, 0]
+   bins[i, 0] = links[cur, 1] # st = st.next
+   if bins[i, 0] < 0:
+      bins[i, 1] = -1 # if st is -1, then ed = -1
+   else:
+      links[bin[i, 0], 0] = -1 # st.prev = -1
+   links[cur, 1] = -1 # cur.next = -1
+   return cur
+
+# attach cur to the beginning of the doubly linked list
+def dll_add_first(i, bins, links, cur):
+   links[cur, 1] = bins[i, 0] # cur.next = st
+   if bins[i, 0] > 0:
+      links[bins[i, 0], 0] = cur # st.prev = cur
+   else:
+      bins[i, 1] = cur # if no st before, st = ed = cur
+   bins[i, 0] = cur
+
+# detach cur from the doubly linked list bins[i]
+def dll_detach_cur(cur, i, bins, links):
+   prev = links[cur, 0]
+   next = links[cur, 1]
+   if prev > 0:
+      links[prev, 1] = next #prev.next = cur.next
+   if next > 0:
+      links[next, 0] = prev #next.prev = cur.prev
+   if bins[i, 0] == cur:
+      bins[i, 0] = next #if st is cur, st = cur.next
+   if bins[i, 1] == cur:
+      bins[i, 1] = prev #if ed is cur, ed = cur.prev
+   links[cur, 0] = links[cur, 1] = -1
+   
+
+ordered = np.empty(g.num_vertices())
+#positions to add vertices 
+head = 0
+tail = g.num_vertices() - 1
+
+while head != tail: 
+   #put all sinks
+   while True:
+      cur = dll_rm_first(0, bins, links)
+      if cur >= 0:
+         ordered[tail--] = cur
+      else:
+         break
+
+   #put all sources
+   while True:
+      cur = dll_rm_first(1, bins, links)
+      if cur >= 0:
+         ordered[head++] = cur
+      else:
+         break
+
+
+   if head == tail:
+      break #finished all vertices
+   
+   #otherwise find nonempty bin with largest diff
+   cur_bin = num_bins - np.argmax(bins[::-1, 0] >= 0) - 1 
+   
+   cur = dll_rm_first(cur_bin, bins, links)
+   ordered[head++] = cur
+
+   cur_out = g.get_out_edges(cur, [g.ep.weight])
+
+   
